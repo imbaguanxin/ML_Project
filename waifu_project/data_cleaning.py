3d55b4254d5bb7050b27ee21de7fdb29d190a473
Xin Guan <imbaguanxin@outlook.com>
2020-04-07 17:21:17 -0400

    update data loader

--- waifu_project/data_cleaning.py
+++ waifu_project/data_cleaning.py
@@ -4,14 +4,17 @@ import shutil
 
 
 def data_cleaning():
-    source_path = 'data_set/self-collected'  # 'data_set/self-collected (copy)'
-    destination_path = 'data_set/modeling_data'
+    source_path = os.path.join('data_set', 'self-collected')  # 'data_set/self-collected (copy)'
+    destination_path = os.path.join('data_set', 'modeling_data')
     source_pics_folders = os.listdir(source_path)
 
     doujin_re = '.*doujin.*'
     official_re = '.*official.*'
     doujin = [x for x in source_pics_folders if re.match(doujin_re, x)]
     official = [x for x in source_pics_folders if re.match(official_re, x)]
+
+    shutil.rmtree(destination_path)
+    os.makedirs(destination_path)
 
     for file in official:
         pic_folder_path = os.path.join(source_path, file)
@@ -26,6 +29,7 @@ def data_cleaning():
             shutil.copy(os.path.join(pic_folder_path, pic), destination_folder)
             os.rename(os.path.join(destination_folder, pic), os.path.join(destination_folder, "official_" + pic))
         print("{} COPY WORK FINISHED".format(file))
+
     for file in doujin:
         pic_folder_path = os.path.join(source_path, file)
         modified_file_name = file[6:-7]
